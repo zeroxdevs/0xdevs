e4d6b55f4852b40aa413a335cc4bdc1bad6d03d6
Mohammad Hossien Lotfazar <fatal@Mohammads-Mac-mini.local>
2023-11-17 00:53:48 +0330

    fix: fix ColorPaletteItem record value

--- src/settings/colors/@types/index.ts
+++ src/settings/colors/@types/index.ts
@@ -2,6 +2,6 @@ export type ColorValue = string;
 
 export type ColorNames = 'primary' | 'secondary' | 'accent' | 'white' | 'black';
 
-export type ColorPaletteItem = Record<ColorNames, string>;
+export type ColorPaletteItem = Record<ColorNames, ColorValue>;
 
 export type ColorPalette = () => ColorPaletteItem;
