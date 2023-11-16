a328b79c7a600ce074620805e7f54a530572bc79
Mohammad Hossien Lotfazar <fatal@Mohammads-Mac-mini.local>
2023-11-16 17:54:29 +0330

    chore: add storybook to project

---
+++ .storybook/theme.ts
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming/create';
+
+const storybookTheme = create({
+  base: 'dark',
+  brandTitle: '0xDevs',
+  brandUrl: 'https://0xdevs.com',
+  brandTarget: '_self',
+  //    brandImage: '',
+});
+
+export default storybookTheme;
