fb6bcd2c7afd0a3429266de0c4637acfb23f1443
Mohammad Hossien Lotfazar <fatal@Mohammads-Mac-mini.local>
2023-11-17 18:25:02 +0330

    fix: commit uncommited staged changes that created using husky pre-hook

--- script/svg2comp/bin.mjs
+++ script/svg2comp/bin.mjs
@@ -1,28 +1,28 @@
 // "npx @svgr/cli --template ./template.js --typescript --out-dir ./src/components/Svg/assets -- ./public/*";
-import inquirer from "inquirer";
-import { execSync } from "child_process";
-import path from "path";
-import fs from "fs";
+import inquirer from 'inquirer';
+import { execSync } from 'child_process';
+import path from 'path';
+import fs from 'fs';
 
-const __dirname = new URL(".", import.meta.url).pathname;
-const __root = path.join(__dirname, "..", "..");
-const sourceDir = path.join(__root, "public", "assets");
-const distDir = path.join(__root, "src", "components", "Svg", "assets");
+const __dirname = new URL('.', import.meta.url).pathname;
+const __root = path.join(__dirname, '..', '..');
+const sourceDir = path.join(__root, 'public', 'assets');
+const distDir = path.join(__root, 'src', 'components', 'Svg', 'assets');
 
 const totalSvgCount = fileCounter(sourceDir);
 
 if (totalSvgCount) {
   const yesNoQuestion = [
     {
-      type: "confirm",
-      name: "convertSvg",
+      type: 'confirm',
+      name: 'convertSvg',
       message: `\x1b[94m${totalSvgCount} \x1b[97m SVG files were found in the "${sourceDir}" ​​folder, would you like to convert it to component?`,
       default: true,
     },
     {
-      type: "confirm",
-      name: "deleteSvg",
-      message: "\x1b[33m Should I delete SVGs after conversion?",
+      type: 'confirm',
+      name: 'deleteSvg',
+      message: '\x1b[33m Should I delete SVGs after conversion?',
       default: false,
     },
   ];
@@ -32,7 +32,7 @@ if (totalSvgCount) {
     if (convertSvg) {
       const command = `npx @svgr/cli --typescript --out-dir ${distDir} -- ${path.join(
         sourceDir,
-        "*"
+        '*',
       )}`;
       console.log(execSync(command).toString());
       if (deleteSvg) {
@@ -41,7 +41,7 @@ if (totalSvgCount) {
     }
   });
 } else {
-  console.log("\x1b[33m", `No SVG files found in "${sourceDir}"`);
+  console.log('\x1b[33m', `No SVG files found in "${sourceDir}"`);
 }
 
 export default function fileCounter(directory) {
@@ -52,7 +52,7 @@ export default function fileCounter(directory) {
     if (fs.statSync(fullPath).isDirectory()) {
       svgCount += fileCounter(fullPath);
     } else {
-      if (path.extname(fullPath).toLowerCase() === ".svg") {
+      if (path.extname(fullPath).toLowerCase() === '.svg') {
         svgCount++;
       }
     }
@@ -69,7 +69,7 @@ function deleteSvgFiles(directory) {
     if (fs.statSync(fullPath).isDirectory()) {
       deleteSvgFiles(fullPath);
     } else {
-      if (path.extname(fullPath).toLowerCase() === ".svg") {
+      if (path.extname(fullPath).toLowerCase() === '.svg') {
         fs.unlinkSync(fullPath);
         console.log(`\x1b[31m Deleted: ${fullPath} \x1b[97m`);
       }
