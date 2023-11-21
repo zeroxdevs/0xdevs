12474859f4e20b36e4e65cbba782531f83b976f4
Mohammad Hossien Lotfazar <fatal@Mohammads-Mac-mini.local>
2023-11-21 10:43:46 +0330

    fix(app): fix hydration and space problem for Team description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,24 +12,23 @@ export default function Home() {
       <Header />
       <Divider variant="primary" />
       <Team />
-
       <Div className="max-w-[55rem] mx-auto pt-[83px]">
         <Typography className="text-center leading-[40px]">
-          Meet{' '}
-          <Typography className="border-b leading-[30px]   inline-block border-accent">
+          Meet&nbsp;
+          <span className="border-b leading-[30px] inline-block border-accent">
             0xdevs
-          </Typography>{' '}
-          – a software powerhouse at the intersection of creativity and
-          technology. Our name blends{' '}
-          <Typography className="border-b leading-[30px]   inline-block border-accent">
+          </span>
+          &nbsp;– a software powerhouse at the intersection of creativity and
+          technology. Our name blends&nbsp;
+          <span className="border-b leading-[30px] inline-block border-accent">
             '0x'
-          </Typography>{' '}
-          from hexadecimal with{' '}
-          <Typography className="border-b leading-[30px]   inline-block border-accent">
+          </span>
+          &nbsp;from hexadecimal with&nbsp;
+          <span className="border-b leading-[30px] inline-block border-accent">
             'devs'
-          </Typography>{' '}
-          for developers, reflecting our versatile skill set. Specializing in
-          web and mobile applications, we infuse each project with youthful
+          </span>
+          &nbsp;for developers, reflecting our versatile skill set. Specializing
+          in web and mobile applications, we infuse each project with youthful
           energy and innovative solutions. With Typescript language ecosystem,
           we're the go-to team for dynamic and impactful software development.
         </Typography>
